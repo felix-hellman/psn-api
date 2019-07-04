f9119f89c264cf917f6a5ad0213c52fc6b5291be
Felix Hellman <felix.hellman@trustly.com>
2019-07-04 11:27:51 +0200

    Fixed setup for api

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='psn-api',
+    version='0.1',
+    packages=['psn-api'],
+    url='',
+    license='',
+    author='istolethis',
+    author_email='istolethisrepo@gmial.com',
+    description='Imsorryomgbutthereisnoinstallinstructions'
+)
+
